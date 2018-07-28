6d63ac90c9b21494e3ace7bb55a22a79bfc4ffb1
lzc <18919099610@163.com>
2018-07-28 16:15:38 +0800

    add ex17,myFun

---
+++ py/ex17.py
@@ -0,0 +1,5 @@
+import myFun as mf
+cearth=mf.cir(6378)
+cmars=mf.cir(3396)
+print "Earth's circuference:",cearth
+print "Mars's circuference:",cmars
---
+++ py/myFun.py
@@ -0,0 +1,7 @@
+import numpy as np
+def mua():
+    print "MUA~"
+def cir(r):
+    r=float(r)
+    c=2*np.pi*r
+    return c
